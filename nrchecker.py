def76a67404f9e73c40c36c619943f76cdb5e65b
Hidetoshi Imai <hidepin@gmail.com>
2018-10-03 22:12:02 +0900

    Add __main__ setting.

--- nrchecker.py
+++ nrchecker.py
@@ -4,11 +4,12 @@
 import argparse
 import os
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--path', default=os.path.relpath(os.getcwd()), help='search path')
-args = parser.parse_args()
-search_path = args.path
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path', default=os.path.relpath(os.getcwd()), help='search path')
+    args = parser.parse_args()
+    search_path = args.path
 
-for foldername, subfolders, filenames in os.walk(args.path):
-    for filename in filenames:
-        print(os.path.join(foldername, filename))
+    for foldername, subfolders, filenames in os.walk(args.path):
+        for filename in filenames:
+            print(os.path.join(foldername, filename))
