4096e7f3020c819cd7163cb0f91d94ba2b107236
Hidetoshi Imai <hidepin@gmail.com>
2018-10-03 22:23:56 +0900

    Add is_target and chk_only_utf8.

--- nrchecker.py
+++ nrchecker.py
@@ -4,6 +4,20 @@
 import argparse
 import os
 
+ignore_list = [
+    '.git',
+    ]
+
+def is_target(path):
+    result=True
+    for ignore in ignore_list:
+        if ignore in path:
+            return False
+    return True
+
+def chk_only_utf8(path):
+    print(filename)
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', default=os.path.relpath(os.getcwd()), help='search path')
@@ -12,4 +26,6 @@ if __name__ == '__main__':
 
     for foldername, subfolders, filenames in os.walk(args.path):
         for filename in filenames:
-            print(os.path.join(foldername, filename))
+            path = os.path.join(foldername, filename)
+            if is_target(path):
+                print(os.path.join(foldername, filename))
