20e913641982a5995d9470b5aee6cbb6665fcce0
Hidetoshi Imai <hidepin@gmail.com>
2018-10-04 08:01:58 +0900

    Update checker.

--- nrchecker.py
+++ nrchecker.py
@@ -2,21 +2,28 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import codecs
 import chardet
 import os
 import re
 
 ignore_list = [
-    '.git',
+    '.git/',
+    '/target/',
     ]
 
-def regex_check(filepath, regex, message):
-    regex = re.compile(regex)
-    with open(filepath, "rb") as f:
-        if regex.search(f.read()):
-            print(filepath + ": " + message)
-            return False
-    return True
+check_list = [
+    [u'\u3000', "fullwidth space error"],
+    ]
+
+def chk_regex(filepath, check_list):
+    regex_list = [re.compile(check[0]) for check in check_list]
+    for regex_index, regex in enumerate(regex_list):
+        with codecs.open(filepath, 'r', 'utf-8') as f:
+            for index, line in enumerate(f):
+                result = regex.search(line)
+                if (result):
+                    print(filepath + ": line:" + ": " + check_list[regex_index][1] + str(index))
 
 def is_target(path):
     result=True
@@ -29,12 +36,16 @@ def chk_only_utf8(filepath):
     with open(filepath, "rb") as f:
         if not chardet.detect(f.read())['encoding'] in ['utf-8', 'ascii']:
             print(filepath + ": Encoding error")
+            return False
+    return True
 
 def chk_newline(filepath):
-    regex_check(filepath, r'\r', "newline error")
-
-def chk_fullwidthspace(filepath):
-    regex_check(filepath, r'　', "newline error")
+    regex = re.compile('\r')
+    with open(filepath, "rb") as f:
+        if regex.search(f.read()):
+            print(filepath + ": " + "newline error")
+            return False
+    return True
 
 if __name__ == '__main__':
 
@@ -46,7 +57,6 @@ if __name__ == '__main__':
     for foldername, subfolders, filenames in os.walk(args.path):
         for filename in sorted(filenames):
             filepath = os.path.join(foldername, filename)
-            if is_target(filepath):
-                chk_only_utf8(filepath)
+            if is_target(filepath) and chk_only_utf8(filepath):
                 chk_newline(filepath)
-                chk_fullwidthspace(filepath)
+                chk_regex(filepath, check_list)
